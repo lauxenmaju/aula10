9d640d951e4c30ea27e65a1c4747db38a968f3d0
lauxenmaju <161649077+lauxenmaju@users.noreply.github.com>
2024-09-27 12:10:58 -0300

    "first commit"

---
+++ AULA10_PROJETO_TAMPLETES_STRINGS/script.js
@@ -0,0 +1,8 @@
+let nome = 'Maria';
+let sobrenome = 'Lauxen';
+let idade = 17;
+
+console.log('Meu nome é', nome,sobrenome,'e minha idade é',idade,'anos')
+
+let mensagem = `Meu nome é ${nome} ${sobrenome} e minha idade é ${idade} anos`;
+console.log(mensagem);
